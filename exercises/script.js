2dea617ea405e90dacdea6f9205b90642715a34a
Krisanta Ojo <KrisyK@Krisantas-MacBook-Air.local>
2018-07-11 17:08:11 -0400

    lol

--- exercises/script.js
+++ exercises/script.js
@@ -3,57 +3,77 @@ document.querySelector('.puzzle-0').addEventListener('click', function() {
 	});
 
 	document.querySelector('.puzzle-1').addEventListener('click', function() {
-		
+		alert('hola mundo');
 	});
 
 	document.querySelector('.puzzle-2').addEventListener('click', function() {
+		document.querySelector('h1').textContent = "Chipotle Mukbang"
 
 	});
 
 	document.querySelector('.puzzle-3').addEventListener('click', function() {
-
+		document.querySelector('img').src = "https://media.giphy.com/media/NQ3SGAMneQ920/giphy.gif";
 	});
 
 	document.querySelector('.puzzle-4').addEventListener('click', function() {
-
+		document.querySelector("img").style.display = "none"
 	});
 
 	document.querySelector('.puzzle-5').addEventListener('click', function() {
-
+		document.querySelector("img").style.display = "block"
 	});
 
 	document.querySelector('.puzzle-6-yellow').addEventListener('click', function() {
-		
+		document.querySelector("html").style.background = "yellow"
 	});
 
 	document.querySelector('.puzzle-6-pink').addEventListener('click', function() {
-		
+		document.querySelector("html").style.background = "pink"
 	});
 
 	document.querySelector('.puzzle-6-purple').addEventListener('click', function() {
-		
+		document.querySelector("html").style.background = " purple"
 	});
 
 	document.querySelector('.puzzle-7').addEventListener('click', function() {
-		
+		document.querySelector('body').style.fontFamily = "Comic Sans MS";
+		document.querySelector("html").style.background = " red"
 	});
 
 	document.querySelector('.puzzle-8').addEventListener('click', function() {
-
+		document.querySelector("a").href= " https://youtu.be/vy8E6LQ9H9Q"
+		document.querySelector("a").textContent= " Styles that NEED to die"
 	});
 
 	// Challenge puzzles
 
-	document.querySelector('.puzzle-9')
+	document.querySelector('.puzzle-9').addEventListener('click', function() {
+		document.querySelector('body').style.fontFamily = "Monaco";
+	});
 
-	document.querySelector('.puzzle-10')
+	document.querySelector('.puzzle-10').addEventListener('click',function () {
+		var audio = new Audio('Gary.mp3');
+		audio.play();
+	});
 
-	document.querySelector('.puzzle-11')
+	document.querySelector('.puzzle-11').addEventListener('click',function () {
+		document.querySelector(".G").innerHTML = "<img src='http://codegakko.com/wp-content/uploads/2016/09/CG-Logo.png'>";
+ 		
 
-	document.querySelector('.puzzle-12')
+	});
+	
+	document.querySelector('.puzzle-12').addEventListener('click', function() {
+		document.querySelector('body').style.color= "blue";
+	});
 
-	document.querySelector('.puzzle-13')
+
+	document.querySelector('.puzzle-13').addEventListener('click', function() {
+		document.querySelector('.ENGLAND').innerHTML= "<marquee>I wish marqee...</marquee>";
+	});
+
+
 
 	document.querySelector('.puzzle-14')
+
 
 	document.querySelector('.puzzle-15')
